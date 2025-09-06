9e75b7ed0ce2af1ebf5085ce88b07f6766b8b859
RHU4N <rhuan3003@gmail.com>
2025-09-06 09:40:56 -0300

    Arquitetura limpa

--- index.js
+++ index.js
@@ -4,26 +4,26 @@ const cors = require('cors');
 const app = express();
 const port = 8081;
 
-const matriz = require('./controllers/matrizController');
-const area = require('./controllers/areaController.js');
-const volume = require('./controllers/volumeController.js') 
-const perimetro = require('./controllers/perimetroController.js')
-const analise = require('./controllers/analiseCombController');
-const angulo = require('./controllers/anguloController');
-const estatistica = require('./controllers/estatisticaController');
-const funcao = require('./controllers/funcaoController');
+const areaRoutes = require('./routes/areaRoutes');
+const matrizRoutes = require('./routes/matrizRoutes');
+const volumeRoutes = require('./routes/volumeRoutes');
+const perimetroRoutes = require('./routes/perimetroRoutes');
+const analiseCombRoutes = require('./routes/analiseCombRoutes');
+const anguloRoutes = require('./routes/anguloRoutes');
+const estatisticaRoutes = require('./routes/estatisticaRoutes');
+const funcaoRoutes = require('./routes/funcaoRoutes');
 
 app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => res.send('Estou aqui'));
-app.use('/matriz', matriz);
-app.use('/area', area);
-app.use('/volume', volume);
-app.use('/perimetro', perimetro);
-app.use('/analise', analise);
-app.use('/angulo', angulo);
-app.use('/estatistica', estatistica);
-app.use('/funcao', funcao);
+app.use('/area', areaRoutes);
+app.use('/matriz', matrizRoutes);
+app.use('/volume', volumeRoutes);
+app.use('/perimetro', perimetroRoutes);
+app.use('/analise', analiseCombRoutes);
+app.use('/angulo', anguloRoutes);
+app.use('/estatistica', estatisticaRoutes);
+app.use('/funcao', funcaoRoutes);
 
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}!`)); // Corrigido o console.log
